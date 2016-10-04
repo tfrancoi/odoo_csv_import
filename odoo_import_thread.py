24047140afbd50d0dbf4b23a839ede51b7c32ab8
Thibault Francois <tfr@openerp.com>
2016-10-04 17:17:02 +0200

    [ADD] Licensing + [FIX] Help

--- odoo_import_thread.py
+++ odoo_import_thread.py
@@ -1,8 +1,22 @@
+#!/usr/bin/env python
 #-*- coding: utf-8 -*-
 '''
 Created on 16 mai 2014
 
-@author: Thibault Francois <francois.th@gmail.com>
+author: Thibault Francois <francois.th@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
 from lib.internal.csv_reader import UnicodeReader, UnicodeWriter
@@ -55,8 +69,8 @@ class RPCThreadImport(RpcThread):
         except Fault as e:
             log_error("Line %s failed" % i)
             log_error(e.faultString)
-        except ValueError:
-            log_error("Line %s failed" % i)
+        except ValueError as e:
+            log_error("Line %s failed value error" % i)
         except Exception as e:
             log_info("Unknown Problem")
             exc_type, exc_value, exc_traceback = sys.exc_info()
@@ -153,17 +167,17 @@ def split_sort(split, data):
     return data, split_index
 
 parser = argparse.ArgumentParser(description='Import data in batch and in parallel')
-parser.add_argument('-c', '--config', dest='config', default="conf/connection.conf", help='Configuration File', required = True)
+parser.add_argument('-c', '--config', dest='config', default="conf/connection.conf", help='Configuration File that contains connection parameters', required = True)
 parser.add_argument('--file', dest='filename', help='File to import', required = True)
-parser.add_argument('--model', dest='model', help='Model to import, if auto try to guess the model from the filename', required = True)
+parser.add_argument('--model', dest='model', help='Model to import', required = True)
 parser.add_argument('--worker', dest='worker', default=1, help='Number of simultaneous connection')
 parser.add_argument('--size', dest='batch_size', default=10, help='Number of line to import per connection')
 parser.add_argument('--skip', dest='skip', default=0, help='Skip until line [SKIP]')
 parser.add_argument('-f', '--fail',action='store_true', dest="fail", help='Fail mode')
-parser.add_argument('-s', '--sep', dest="seprator", default=";", help='Fail mode')
+parser.add_argument('-s', '--sep', dest="seprator", default=";", help='CSV separator')
 parser.add_argument('--groupby', dest='split', help='Group data per batch with the same value for the given column in order to avoid concurrent update error')
-parser.add_argument('--ignore', dest='ignore', help='Keep batch same value of the field in the same batch')
-parser.add_argument('--check', dest='check', action='store_true', help='Check if record are imported after each batch. Can slow down the process')
+parser.add_argument('--ignore', dest='ignore', help='list of column separate by comma. Those column will be remove from the import request')
+parser.add_argument('--check', dest='check', action='store_true', help='Check if record are imported after each batch.')
 parser.add_argument('--context', dest='context', help='context that will be passed to the load function, need to be a valid python dict', default="{'tracking_disable' : True}")
 #TODO args : encoding
 #{'update_many2many': True,'tracking_disable' : True, 'create_product_variant' : True, 'check_move_validity' : False}
@@ -227,4 +241,4 @@ while  i < len(data):
 rpc_thread.wait()
 file_result.close()
 
-log_info("%s %s imported : total time %s second(s)" % (len(data), model, (time() - st)))
+log_info("%s %s imported, total time %s second(s)" % (len(data), model, (time() - st)))
