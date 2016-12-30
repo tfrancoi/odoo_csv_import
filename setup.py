8889c5daf15451e31e9d2d581892bcb0fd97bdfb
Thibault Francois <francois.th@gmail.com>
2016-12-30 01:04:29 +0100

    [ADD] setup.py use to upload the package on pypi package index

---
+++ setup.py
@@ -0,0 +1,37 @@
+#-*- coding: utf-8 -*-
+'''
+Copyright (C) Thibault Francois
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation, version 3.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Lesser Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+'''
+
+from setuptools import setup, find_packages
+
+
+setup(name='odoo-import-export-client',
+      version='1.0.0',
+      install_requires=['openerp-client-lib'],
+      description='Library and script that allow to export and import data to Odoo using rpc api.',
+      author='Thibault Francois',
+      author_email='thibault@françois.be',
+      url='https://github.com/tfrancoi/odoo_csv_import',
+      packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+      scripts=['odoo_export_thread.py', 'odoo_import_thread.py'],
+      long_description="See the home page for any information: https://github.com/tfrancoi/odoo_csv_import",
+      keywords="odoo library import export thread python client lib web service",
+      license="LGPLv3",
+      classifiers=[
+          "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+          "Programming Language :: Python",
+          ],
+     )
