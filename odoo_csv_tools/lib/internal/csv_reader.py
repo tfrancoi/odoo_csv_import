a4f6ce13dbbc0cb9bbae8cb6f64d243cd7f69cc1
Thibault Francois <francois.th@gmail.com>
2017-02-09 15:06:24 +0100

    [FIX] encoding in utf-8 of boolean failed

--- odoo_csv_tools/lib/internal/csv_reader.py
+++ odoo_csv_tools/lib/internal/csv_reader.py
@@ -53,7 +53,7 @@ class UnicodeWriter:
 
     def writerow(self, row):
         self.lock.acquire()
-        self.writer.writerow([s.encode("utf-8") for s in row])
+        self.writer.writerow([str(s) if isinstance(s, bool) else s.encode("utf-8") for s in row])
         # Fetch UTF-8 output from the queue ...
         data = self.queue.getvalue()
         data = data.decode("utf-8")
