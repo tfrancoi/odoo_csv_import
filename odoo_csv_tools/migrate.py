8ef63882238bb3ac53ca9ccb4133ab1d0b152edf
Thibault Francois <francois.th@gmail.com>
2026-02-08 12:31:44 +0100

    [FIX] fix migrate tools
    Boolean are export as bool not as string that make the import crash
    Fix relative import

--- odoo_csv_tools/migrate.py
+++ odoo_csv_tools/migrate.py
@@ -5,9 +5,9 @@ Created on 27 déc. 2016
 
 @author: Thibault Francois
 '''
-from lib.transform import Processor
-from export_threaded import export_data
-from import_threaded import import_data
+from .lib.transform import Processor
+from .export_threaded import export_data
+from .import_threaded import import_data
 
 class Migrator(object):
 
@@ -19,11 +19,18 @@ class Migrator(object):
         self.export_batch_size = 100
         self.export_max_con = 1
 
-    def migrate(self, model, domain, field_export, mappings=[None]):
-        header, data = export_data(self.config_export, model, domain, field_export, max_connection=self.export_max_con, batch_size=self.export_batch_size)
+    def stringify(self, data):
+        new_data = []
+        for d in data:
+            new_data.append([str(item) for item in d])
+        return new_data
+
+    def migrate(self, model, domain, field_export, mappings=[None], context_out=None, context_in=None):
+        header, data = export_data(self.config_export, model, domain, field_export, max_connection=self.export_max_con, batch_size=self.export_batch_size, context=context_out)
+        data = self.stringify(data)
         processor = Processor(header=header, data=data)
         for mapping in mappings:
             if not mapping:
                 mapping = processor.get_o2o_mapping()
             to_import_header, to_import_data = processor.process(mapping, False, {})
-            import_data(self.config_import, model, header=to_import_header, data=to_import_data, max_connection=self.import_max_con, batch_size=self.import_batch_size)
+            import_data(self.config_import, model, header=list(to_import_header), data=to_import_data, max_connection=self.import_max_con, batch_size=self.import_batch_size, context=context_in)
